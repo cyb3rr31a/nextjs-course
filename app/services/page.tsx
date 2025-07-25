33f89afc3453d01aa4d2fe5a2bedc2df0836613d
Rebecca Shirievo <139311697+cyb3rr31a@users.noreply.github.com>
2025-07-25 15:58:22 +0300

    add: services page

---
+++ app/services/page.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: 'Services | Portfolio',
+  description: 'These are the services I offer'
+};
+
+export default function ServicePage() {
+    return <h1 className="text-2xl"> My Services</h1>
+}
