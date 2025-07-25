89cdcd6cf72beae262f3f9bd699545ef591f5386
Rebecca Shirievo <139311697+cyb3rr31a@users.noreply.github.com>
2025-07-25 15:55:49 +0300

    add: about us page

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+    title: 'About Us | Next.js Course',
+    description: 'Learn about our mission in this guided Next.js course.'
+};
+
+export default function AboutPage() {
+    return <h1 className="text-2xl">About Us!</h1>
+}
