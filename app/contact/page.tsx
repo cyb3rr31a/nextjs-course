f023865b7d43c1942e624ec0367e2ab45394f135
Rebecca Shirievo <139311697+cyb3rr31a@users.noreply.github.com>
2025-07-25 15:56:54 +0300

    add: contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: 'Contact | Portfolio',
+  description: 'Contact me on these platforms',
+};
+
+export default function ContactPage() {
+    return <h1 className="text-2xl">Contact Me</h1>
+}
