b17d5589989f60d39842f49289325a48d068252f
Rebecca Shirievo <139311697+cyb3rr31a@users.noreply.github.com>
2025-08-04 12:36:53 +0300

    update: add metadata

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,8 @@
+export const metadata = {
+  title: 'Dashboard | Next.js Course',
+  description: 'Dashboard Page',
+};
+
 export default function DashboardPage() {
     return <h1 className="text-2xl">Dashboard Page</h1>
 } 
